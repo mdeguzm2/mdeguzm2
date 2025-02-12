459a295f18fc2110aa72e40af8a9a9bc3051e50b
mdeguzm2 <deg.matthew@gmail.com>
2025-02-12 15:25:24 -0500

    fixed files
    remade python files since interpreter wasnt working properly on old one

--- BankAccount1/SavingsAccount.py
+++ BankAccount1/SavingsAccount.py
@@ -3,4 +3,4 @@ import BankAccount
 class SavingsAccount(BankAccount):
     def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
         super().__init__(customer_name, current_balance, minimum_balance)
-    self.interest_rate
+
