8dc2029615e96892821d1b6c061ad0eda9098ab0
mdeguzm2 <deg.matthew@gmail.com>
2025-02-12 17:55:30 -0500

    fixed inheritance issues
    fixed the inheritance issues coming from the import lines for both classes

--- BankAccount/CheckingAccount.py
+++ BankAccount/CheckingAccount.py
@@ -1,6 +1,18 @@
-import BankAccount
+from BankAccount import BankAccount
 
 class CheckingAccount(BankAccount):
     def __init__(self, customer_name, current_balance, minimum_balance, maximum_transfer):
         super().__init__(customer_name, current_balance, minimum_balance)
         self.maximum_transfer = maximum_transfer
+
+    def withdraw(self, amount):
+        if ((self.current_balance-self.minimum_balance) >= amount) and (amount <= self.maximum_transfer):
+            self.current_balance -= amount
+            print("Successfully withdrawn $" + str(amount))
+        else:
+            print("Could not withdraw due to reaching below minimum balance or withdrawing"
+                  " more than maximum transfer")
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print("Maximum transfer limit: $" + str(self.maximum_transfer))
--- BankAccount/SavingsAccount.py
+++ BankAccount/SavingsAccount.py
@@ -1,6 +1,11 @@
-import BankAccount
+from BankAccount import BankAccount
 
 class SavingsAccount(BankAccount):
     def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
         super().__init__(customer_name, current_balance, minimum_balance)
         self.interest_rate = interest_rate
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print("Interest Rate: {}".format(self.interest_rate))
+        print("Balance after next month: {}".format(self.current_balance*(1+self.interest_rate)))
