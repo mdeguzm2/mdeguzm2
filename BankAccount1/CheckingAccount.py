d1e08147dc5ae23f05db9846ffbf31f6f29713eb
mdeguzm2 <deg.matthew@gmail.com>
2025-02-12 18:01:15 -0500

    removed not working files

--- BankAccount1/CheckingAccount.py
+++
@@ -1,5 +0,0 @@
-import BankAccount
-
-class CheckingAccount(BankAccount):
-    def __init__(self, customer_name, current_balance, minimum_balance):
-        super().__init__(customer_name, current_balance, minimum_balance)
