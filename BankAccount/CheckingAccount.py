7e6c2118d87d19354f9c856349dcd139f3ba9cf7
mdeguzm2 <deg.matthew@gmail.com>
2025-02-12 15:29:33 -0500

    added new parameters
    added the maximum_transfer and interest rate parameters

--- BankAccount/CheckingAccount.py
+++ BankAccount/CheckingAccount.py
@@ -1,5 +1,6 @@
 import BankAccount
 
 class CheckingAccount(BankAccount):
-    def __init__(self, customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance, maximum_transfer):
         super().__init__(customer_name, current_balance, minimum_balance)
+        self.maximum_transfer = maximum_transfer
